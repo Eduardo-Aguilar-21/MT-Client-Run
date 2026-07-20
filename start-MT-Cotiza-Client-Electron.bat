87750e7918e26c0ceea37301be52952b6203627a
Eduardo Aguilar <es123y123@gmail.com>
2026-07-20 10:36:41 -0500

    Clean Run processes and logs before Electron start

--- start-MT-Cotiza-Client-Electron.bat
+++ start-MT-Cotiza-Client-Electron.bat
@@ -1,6 +1,9 @@
 @echo off
 setlocal
 cd /d "%~dp0"
+
+powershell.exe -NoProfile -ExecutionPolicy Bypass -Command "Get-Process java,node,postgres,electron -ErrorAction SilentlyContinue | Where-Object { $_.Path -like '%CD%\*' } | Stop-Process -Force -ErrorAction SilentlyContinue; Remove-Item -Recurse -Force '.\data\logs' -ErrorAction SilentlyContinue"
+
 if not exist "electron\dist\electron.exe" (
   call "%~dp0prepare-MT-Cotiza-Client-Run-Electron.bat"
   if errorlevel 1 exit /b 1
